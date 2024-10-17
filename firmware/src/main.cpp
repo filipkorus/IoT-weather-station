7b490b2d8e9704952ae8d0961efb47b31cdc86c4
Natabyc <nataliabyc01@gmail.com>
2024-10-17 15:50:20 +0200

    libs and basic tests (#3)

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,6 +1,8 @@
 #include <Arduino.h>
 #include <FastLED.h>
-#include <RadioLib.h>
+#include <Wire.h>
+#include <LIDARLite.h>
+LIDARLite myLidarLite;
 
 #define NUM_LEDS 1
 #define DATA_PIN 0
@@ -8,16 +10,25 @@ CRGB leds[NUM_LEDS];
 
 void setup()
 {
+  Serial.begin(9600);
+
   FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
   leds[0] = CRGB::Black;
   FastLED.show();
 
-  SPI.begin(5, 19, 27, 17);
-  SX1272 radio = new Module(17, 23, 18, 22);
+  Serial.println();
 
-  radio.begin();
+  myLidarLite.begin(0, true);
+  myLidarLite.configure(0);
 }
 
 void loop()
 {
+  Serial.println(myLidarLite.distance());
+
+  // Take 99 measurements without receiver bias correction and print to serial terminal
+  for (int i = 0; i < 99; i++)
+  {
+    Serial.println(myLidarLite.distance(false));
+  }
 }
