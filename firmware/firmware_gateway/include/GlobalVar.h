5b8ed323e0f0c464a2d8af791cc72f023530f33a
Mateusz Jakubowski <matikd1324@gmail.com>
2024-11-14 14:31:38 +0100

    kod gateway (#28)

---
+++ firmware/firmware_gateway/include/GlobalVar.h
@@ -0,0 +1,9 @@
+#ifndef GLOBALVAR_H
+#define GLOBALVAR_H
+
+#define BACKEND_IP "iot.fkor.us"
+#define AUTH_TOKEN "0Y7wRO5yDhvS2pHsASkgwcJcRTe0BQDd0fztHVHjZ9Tfk6aMgWmwYIulcyw550byKw3ovJePv56H8e31khq2FUomxcrKSe9GfiIi10zJBHkCXLvmKvu7CWKGyyLH9mFA"
+#define UID "N202411131A"
+#define WSS true
+
+#endif
