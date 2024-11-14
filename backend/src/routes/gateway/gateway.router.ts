78f02a411a4515f45b791d870544fc45b82d5cf2
Filip Korus <filipkorus123@gmail.com>
2024-11-14 12:17:47 +0100

    add latitude and longitude to the gw

--- backend/src/routes/gateway/gateway.router.ts
+++ backend/src/routes/gateway/gateway.router.ts
@@ -2,7 +2,7 @@ import {Router} from 'express';
 import {
 	GetGatewayHandler,
 	InitGatewayHandler,
-	RenameGatewayHandler,
+	UpdateGatewayHandler,
 	GatewayPairingCodeHandler
 } from './gateway.controller';
 import RequireAuth from '../../middlewares/requireAuth';
@@ -12,7 +12,7 @@ const router = Router();
 
 router.get('/', RequireAuth, GetGatewayHandler);
 router.get('/:id', RequireAuth, GetGatewayHandler);
-router.put('/:id', RequireAuth, RenameGatewayHandler);
+router.put('/:id', RequireAuth, UpdateGatewayHandler);
 router.post('/pariring-code', RequireAuth, GatewayPairingCodeHandler);
 
 // endpoints for the Gateway itself
