094e670e54c3d116ed0495181ae3f8826e127cc7
Filip Korus <filipkorus123@gmail.com>
2024-11-28 07:15:20 +0100

    PWA

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,51 @@
 // vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import {VitePWA} from 'vite-plugin-pwa';
 
 export default defineConfig({
-    plugins: [react()],
+    plugins: [
+       react(),
+        VitePWA({
+            registerType: 'autoUpdate',
+            devOptions: {
+                enabled: false, // Enable during development
+            },
+            manifest: {
+                name: 'Ski Weather Station',
+                short_name: 'Ski Weather',
+                description: 'Stacja pogodowa dla stoków narciarskich',
+                theme_color: '#ffffff',
+                background_color: '#ffffff',
+                icons: [
+                    {
+                        "src": "/icons/pwa-192x192.png",
+                        "sizes": "192x192",
+                        "type": "image/png",
+                        "purpose": "any"
+                    },
+                    {
+                        "src": "/icons/pwa-512x512.png",
+                        "sizes": "512x512",
+                        "type": "image/png",
+                        "purpose": "any"
+                    },
+                    {
+                        "src": "/icons/pwa-maskable-192x192.png",
+                        "sizes": "192x192",
+                        "type": "image/png",
+                        "purpose": "maskable"
+                    },
+                    {
+                        "src": "/icons/pwa-maskable-512x512.png",
+                        "sizes": "512x512",
+                        "type": "image/png",
+                        "purpose": "maskable"
+                    }
+                ],
+            },
+        }),
+    ],
     resolve: {
         alias: {
             // Map the custom paths to their respective directories
